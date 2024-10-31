de2bbf86649230b22789e11ca95311c0b7c2b89e
Pradyu <pvasu009@uottawa.ca>
2024-10-31 14:50:45 -0400

    init commit

---
+++ src/main/kotlin/seg3x02/bookrestapi/BookrestapiApplication.kt
@@ -0,0 +1,11 @@
+package seg3x02.bookrestapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BookrestapiApplication
+
+fun main(args: Array<String>) {
+	runApplication<BookrestapiApplication>(*args)
+}
---
+++ src/test/kotlin/seg3x02/bookrestapi/BookrestapiApplicationTests.kt
@@ -0,0 +1,13 @@
+package seg3x02.bookrestapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BookrestapiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
